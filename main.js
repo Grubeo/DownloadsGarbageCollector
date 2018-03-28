827b6b415b3d0cc8d32c3f01a365f0d38d6eb5bb
Grubeo <robben_1@wp.pl>
2018-03-28 22:34:10 +0200

    Addding support for linux system

--- main.js
+++ main.js
@@ -1,35 +1,21 @@
 const fs = require('fs')
-const errorPath = 'c:\\etc\\downloads-gc\\errors.txt'
-const downloadDirPath = process.env.HOMEDRIVE + process.env.HOMEPATH + '\\Downloads\\'
-const directoryContent = fs.readdirSync(downloadDirPath)
-const config = JSON.parse(fs.readFileSync('config.json', {encoding: 'utf8'}))
+const sorter = require('./sorter.js')
 
-function extensionToCategory(extension) {
-    extension = extension.toUpperCase()
-    return config.find(entry => entry.fileType.find(x => x === extension))
+const config = JSON.parse(fs.readFileSync('./config.json'))
+
+
+function platformDefinedData(platform) {
+    return {
+        win32: {
+            separator: '\\',
+            downloadsPath: process.env.HOMEDRIVE + process.env.HOMEPATH + '\\' + config.root
+        },
+        linux: {
+            separator: '/',
+            downloadsPath: process.env.HOME + '/' + config.root
+        }
+    }[platform]
 }
 
-for (let file of directoryContent) {
-    const extension = /[\S\s]+\.(\S+)/g.exec(file)
-    if (!extension)
-        continue
-
-    const category = extensionToCategory(extension[1])
-    if (category) {
-        const path = downloadDirPath + extensionToCategory(extension[1])
-
-        if (!fs.existsSync(path))
-            fs.mkdirSync(path)
-        
-        const src = downloadDirPath + file
-        const dst = path + '\\' + file
-
-        console.log(`Copying ${src} to ${dst}`)
-
-        fs.copyFile(src, dst, error => {
-            if (!error)
-                fs.unlink(src, error =>
-                    (fs.existsSync(errorPath) ? fs.appendFileSync : fs.writeFileSync)(errorPath, error.toString() + '\n'))
-        })
-    }
-}
+const platformDepended = platformDefinedData(process.platform)
+sorter(platformDepended, config.directories)
---
+++ sorter.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const errorPath = 'errors.txt'
+
+module.exports = function(systemInfo, directories) {
+    console.log(systemInfo)
+    const directoryContent = fs.readdirSync(systemInfo.downloadsPath)
+
+    function extensionToCategory(extension) {
+        extension = extension.toUpperCase()
+        return directories.find(entry => entry.fileType.find(x => x === extension))
+    }
+
+    for (let file of directoryContent) {
+        const extension = /[\S\s]+\.(\S+)/g.exec(file)
+        if (!extension)
+            continue
+
+        const category = extensionToCategory(extension[1])
+        if (category) {
+            const path = systemInfo.downloadsPath + systemInfo.separator + extensionToCategory(extension[1]).name
+            if (!fs.existsSync(path))
+                fs.mkdirSync(path)
+            
+            const src = systemInfo.downloadsPath + systemInfo.separator + file
+            const dst = path + systemInfo.separator + file
+
+            console.log(`Copying ${src} to ${dst}`)
+
+            fs.copyFile(src, dst, error => {
+                if (!error)
+                    fs.unlink(src, error =>
+                        (fs.existsSync(errorPath) ? fs.appendFileSync : fs.writeFileSync)(errorPath, error + '\n'))
+            })
+        }
+    }
+}
